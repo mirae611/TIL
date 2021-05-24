6a21818266b9e37caaa16f241fef5e98487c39d7
장미래 <jangmirae611@gmail.com>
2021-05-24 18:16:34 +0900

    프론트엔드실무완성 - UI컴포넌트 case1_conTextMenu Q2

--- Front-end/Front-end Project/case1_context-menu/question/q2_jquery/index.js
+++ Front-end/Front-end Project/case1_context-menu/question/q2_jquery/index.js
@@ -3,3 +3,29 @@ import "./style.css";
 import $ from "jquery";
 
 // Write JQuery code here!
+// solution 1
+const $wrapper = $(".wrapper");
+const $items = $(".item");
+
+$wrapper.on("click", ".item", function (e) {
+  // '.item'이 e.target의 역할
+  e.stopPropagation();
+  $(this).toggleClass("open").siblings().removeClass("open");
+});
+
+$("body").on("click", function (e) {
+  $items.removeClass("open");
+});
+
+// solution 1-1
+// 리스너 줄이기
+const $items = $(".item");
+
+$("body").on("click", function (e) {
+  const item = $(e.target);
+  if (item.is(".item")) {
+    item.toggleClass("open").siblings().removeClass("open");
+  } else {
+    $items.removeClass("open");
+  }
+});
