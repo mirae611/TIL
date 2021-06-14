bf48293fda4073884bb37e87ae183988a0e00308
mirae <mirae611@nate.com>
2021-06-14 17:28:23 +0900

    EventBus 사용

--- Front-end/Vue/Web-Game-with-Vue/tictactoe/TicTacToe.vue
+++ Front-end/Vue/Web-Game-with-Vue/tictactoe/TicTacToe.vue
@@ -8,6 +8,7 @@
 
 <script>
 import TableComponent from './TableComponent';
+import EventBus from './EventBus';
 
 export default {
   components: {
@@ -23,6 +24,51 @@ export default {
       turn: 'O', // O, X
       winner: '',
     }
+  },
+  methods: {
+    onClickTd(rowIndex, cellIndex) {
+      let win = false;
+      this.$set(this.tableData[rowIndex], cellIndex, this.turn);
+
+      if (this.tableData[rowIndex][0] === this.turn && this.tableData[rowIndex][1] === this.turn && this.tableData[rowIndex][2] === this.turn) {
+        win = true;
+      }
+      if (this.tableData[0][cellIndex] === this.turn && this.tableData[1][cellIndex] === this.turn && this.tableData[2][cellIndex] === this.turn) {
+        win = true;
+      }
+      if (this.tableData[0][0] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][2] === this.turn) {
+        win = true;
+      }
+      if (this.tableData[0][2] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][0] === this.turn) {
+        win = true;
+      }
+
+      if (win) { // 이긴 경우 : 3줄 달성
+        this.winner = this.turn; // 승자 기록
+        // 데이터 초기화
+        this.turn = 'O';
+        this.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+      } else { //  무승부
+        let all = true;
+        this.tableData.forEach(row => { // 무승부 검사
+          row.forEach(cell => {
+            if(!cell) {
+              all = false;
+            }
+          });
+        });
+        if (all) { // 무승부
+          this.winner = '';
+          this.turn = 'O';
+          this.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+        } else { // 아직 게임이 끝나지 않은 경우
+          this.turn = this.turn === 'O' ? 'X' : 'O'; // 턴 바꿔주기
+        }
+      }
+    }
+  },
+  created() {
+    EventBus.$on('clickTd', this.onClickTd);
   }
 }
 </script>
